c5242f18278e9bbd66b1f6d310cb0ac3f1c04c37
Jilroge7 <1672@holbertonschool.com>
2020-08-11 20:07:59 +0000

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -7,19 +7,20 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         if not kwargs:
-           from models import storage
-           # id = Column(String(60), nullable=False, primary_key=True)
-           # created_at = Column(Datetime, default=datetime.utcnow())
-           # updated_at = Column(Datetime, default=datetime.utcnow())
-           self.id = str(uuid.uuid4())
-           self.created_at = datetime.now()
-           self.updated_at = datetime.now()
-           storage.new(self)
+            from models import storage
+            # id = Column(String(60), nullable=False, primary_key=True)
+            # created_at = Column(Datetime, default=datetime.utcnow())
+            # updated_at = Column(Datetime, default=datetime.utcnow())
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
