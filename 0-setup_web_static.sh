7ad7cd5dd73f8f5a5d985dc62bef49ff774b9d12
Jacob Ide <jacobmeide@gmail.com>
2020-08-17 23:00:35 +0000

    4

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,8 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "<h1>Holberton Test</h1>" > /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 sed -i "38a location /hbnb_static {\nalias /data/web_static/current/;\n}" $FILE
 service nginx restart
