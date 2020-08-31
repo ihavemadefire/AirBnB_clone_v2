290c4444ad78c7ab661d9961dcaabb82aab96777
Jacob Ide <jacobmeide@gmail.com>
2020-08-31 15:48:37 +0000

    Add proper directory

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Launches simple flask app
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+    strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB_ext():
+    strict_slashes = False
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',
+            port=5000,
+            debug=True)
