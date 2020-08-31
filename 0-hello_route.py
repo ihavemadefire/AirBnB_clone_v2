a2070b4f7946f6cbbd6da23b81189b4f8507ee44
Jacob Ide <jacobmeide@gmail.com>
2020-08-31 15:12:18 +0000

    0

--- 0-hello_route.py
+++ 0-hello_route.py
@@ -4,9 +4,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_HBNB():
-    strict_slashes=False
+    strict_slashes = False
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
