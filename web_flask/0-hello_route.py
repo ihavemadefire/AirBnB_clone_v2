ca7099de9fc5e0cea2890c6ccd40b6183626ae80
Jacob Ide <jacobmeide@gmail.com>
2020-08-31 18:52:20 +0000

    Change where strict slashes is set, submit 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-# Launches simple flask app
+"""DOCSTRING"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_HBNB():
-    strict_slashes = False
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
-            port=5000,
-            debug=True)
+            port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Launches simple flask app
+"""DOCSTRING"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,14 +7,21 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
+    strict_slashes = False
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB_ext():
+    strict_slashes = False
     return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def HBNB_text(text):
+    text1 = text.replace("_", " ")
+    return ("C {}".format(text1))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
-            port=5000,
-            debug=True)
+            port=5000)
