85c8531080bd28996840407aad92d0567275a5f6
Jacob Ide <jacobmeide@gmail.com>
2020-08-31 20:39:40 +0000

    First template rendered

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defines the routhing for a simple flask app"""
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """This routes to the root page"""
+    strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB_ext():
+    """This routes to another page"""
+    strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def HBNB_text(text):
+    """This accepts text as input and renders the result"""
+    text1 = text.replace("_", " ")
+    return ("C {}".format(text1))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def HBNB_text_python(text="is cool"):
+    """This routes to a file, but set a default value"""
+    text1 = text.replace("_", " ")
+    return ("Python {}".format(text1))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def HBNB_is_int(n):
+    """This accepts an int as input and renders the result if int"""
+    return ("{} is a number".format(n))
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def HBNB_is_int_template(n):
+    """This accepts an int as input and renders html if int"""
+    return render_template("5-number.html", n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',
+            port=5000,
+            debug=True)
