1cf5f1bbf69ebebae8a043b767acfd65bb33f2b8
Jacob Ide <jacobmeide@gmail.com>
2020-08-31 15:11:07 +0000

    0

---
+++ 0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Launches simple flask app
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_HBNB():
+    strict_slashes=False
+    return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',
+            port=5000,
+            debug=True)
